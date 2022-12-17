dd0879a9682e47c97887a7fa4459e71985ee9470
Avery Griffin <averyzanegriffin@gmail.com>
2022-12-16 21:43:43 -0500

    Updated hyperparameters

--- Chapter1_Transformers/training.py
+++ Chapter1_Transformers/training.py
@@ -18,31 +18,33 @@ def get_label(text, tokenizer, max_sequence):
     return torch.nn.functional.one_hot(token_id, tokenizer.vocab_size).float()
 
 
+device = torch.device("cuda:0")
+
 train = WikiText2(split='train')
-max_sequence = 32
+max_sequence = 256
 tokenizer = AutoTokenizer.from_pretrained("gpt2", model_max_length=max_sequence)
 tokenizer.add_special_tokens({'pad_token': '[PAD]'})
 embedding_model = AutoModel.from_pretrained("gpt2")
 embedding_model.resize_token_embeddings(len(tokenizer))
 pipe = pipeline('feature-extraction', model=embedding_model, tokenizer=tokenizer, padding=True, truncation=True)
 
-device = torch.device("cuda:0")
-model = GPTAve(num_decoders=1, d=embedding_model.embed_dim, f=embedding_model.embed_dim, vocab_size=tokenizer.vocab_size).to(device)
+model = GPTAve(num_decoders=1, d=embedding_model.embed_dim, f=embedding_model.embed_dim, heads=12, vocab_size=tokenizer.vocab_size).to(device)
 print(model)
 loss_fc = torch.nn.CrossEntropyLoss()
-opt = torch.optim.Adam(lr=1e-3, params=model.parameters())
+opt = torch.optim.Adam(lr=1e-5, params=model.parameters())
 
 s = 0
 for text in train:
     x = preprocess_text(text, pipe).to(device)
-    y = get_label(text, tokenizer, max_sequence).to(device)
-    opt.zero_grad()  # clears gradients
-    logits = model(x)
-    loss = loss_fc(logits, y)
-    loss.backward()  # compute gradients
-    opt.step()  # update weights
-    print(f"Step {s} Loss {loss}")
-    s += 1
+    if len(x) > 1:
+        y = get_label(text, tokenizer, max_sequence).to(device)
+        opt.zero_grad()  # clears gradients
+        logits = model(x)
+        loss = loss_fc(logits, y)
+        loss.backward()  # compute gradients
+        opt.step()  # update weights
+        print(f"Step {s} Loss {loss}")
+        s += 1
 
 
 
